3d1da033ffe121b7fa63da63ce711d1290be0bb1
TawanMenezes <tawanmenezes@gmail.com>
2022-11-05 13:13:11 -0300

    functions

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
-const display = document.querySelector('.display').innerHTML
+let display = document.querySelector('.display')
 
 const sum = (a,b)=>a+b
 const div = (a,b)=>a/b
 const sub = (a,b)=>a-b
 const mul = (a,b)=>a*b
 
-const ac = () => display = ' '
+// const ac = () => display = 'oi'
+function ac(){
+  display.innerHTML = ''
+  
+}
+function one(){
+  display.innerHTML += '1'
+}
+function two(){
+  display.innerHTML += '2'
+}
+
+function three(){
+  display.innerHTML += '3'
+} 
+function four(){
+  display.innerHTML += '4'
+} 
+function five(){
+  display.innerHTML += '5'
+} 
+function six(){
+  display.innerHTML += '6'
+} 
+function seven(){
+  display.innerHTML += '7'
+} 
+function eight(){
+  display.innerHTML += '8'
+} 
+function nine(){
+  display.innerHTML += '9'
+} 
+function zero(){
+  display.innerHTML += '0'
+} 
