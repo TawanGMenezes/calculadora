dbdaad765b0648d6e8bde95e80e3ea135eb8cc55
TawanMenezes <tawanmenezes@gmail.com>
2022-11-05 12:57:00 -0300

    começando js

---
+++ script.js
@@ -0,0 +1,8 @@
+const display = document.querySelector('.display').innerHTML
+
+const sum = (a,b)=>a+b
+const div = (a,b)=>a/b
+const sub = (a,b)=>a-b
+const mul = (a,b)=>a*b
+
+const ac = () => display = ' '
